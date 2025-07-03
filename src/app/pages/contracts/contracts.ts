9c109ac1be846a56d53d6c6adfd28dacf40870a6
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2025-07-03 17:21:37 -0400

    feat: added results section

--- src/app/pages/contracts/contracts.ts
+++ src/app/pages/contracts/contracts.ts
@@ -1,18 +1,38 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import {
   SearchForm,
   SearchCriteria,
 } from '../../shared/search-form/search-form';
+import { ContractService } from '../../services/contract';
+import { Contract } from '../../models/contract';
 
 @Component({
   selector: 'app-contracts',
-  imports: [SearchForm],
+  imports: [SearchForm, CommonModule],
   templateUrl: './contracts.html',
   styleUrl: './contracts.scss',
 })
 export class Contracts {
+  contracts: Contract[] = [];
+  isLoading = false;
+  hasSearched = false;
+
+  constructor(private contractService: ContractService) {}
+
   onSearchSubmitted(criteria: SearchCriteria) {
-    console.log('Contract search criteria:', criteria);
-    // TODO: Implement actual search logic
+    this.isLoading = true;
+    this.hasSearched = true;
+
+    this.contractService.searchContracts(criteria).subscribe({
+      next: (results) => {
+        this.contracts = results;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.error('Error searching contracts:', error);
+        this.isLoading = false;
+      },
+    });
   }
 }
--- src/app/pages/grants/grants.ts
+++ src/app/pages/grants/grants.ts
@@ -1,18 +1,38 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import {
   SearchForm,
   SearchCriteria,
 } from '../../shared/search-form/search-form';
+import { GrantService } from '../../services/grant';
+import { Grant } from '../../models/grant';
 
 @Component({
   selector: 'app-grants',
-  imports: [SearchForm],
+  imports: [SearchForm, CommonModule],
   templateUrl: './grants.html',
   styleUrl: './grants.scss',
 })
 export class Grants {
+  grants: Grant[] = [];
+  isLoading = false;
+  hasSearched = false;
+
+  constructor(private grantService: GrantService) {}
+
   onSearchSubmitted(criteria: SearchCriteria) {
-    console.log('Grant search criteria:', criteria);
-    // TODO: Implement actual search logic
+    this.isLoading = true;
+    this.hasSearched = true;
+
+    this.grantService.searchGrants(criteria).subscribe({
+      next: (results) => {
+        this.grants = results;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.error('Error searching grants:', error);
+        this.isLoading = false;
+      },
+    });
   }
 }
